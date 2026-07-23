15cf7a96bfed345b72d6ee87926e3a97002fed12
Juninho Alves <junioralv290302@gmail.com>
2026-07-23 08:58:31 -0300

    ajuste

--- js/ui.js
+++ js/ui.js
@@ -1,39 +1,47 @@
-import api from "./api.js";
+import api from "./api.js"
 
 const ui = {
+
+  limparFormulario() {
+    document.getElementById("pensamento-form").reset();
+  },
+  
   async renderizarPensamentos() {
-    const listaPensamentos = document.getElementById("lista-pensamentos");
+    const listaPensamentos = document.getElementById("lista-pensamentos")
 
     try {
-      const pensamentos = await api.buscarPensamentos();
-      pensamentos.forEach((pensamento) => {});
-    } catch {}
+      const pensamentos = await api.buscarPensamentos()
+      pensamentos.forEach(ui.adicionarPensamentoNaLista)
+    }
+    catch {
+      alert('Erro ao renderizar pensamentos')
+    }
   },
 
   adicionarPensamentoNaLista(pensamento) {
-    const listaPensamentos = document.getElementById("lista-pensamentos");
-    const li = document.createElement("li");
-    li.setAttribute("data-id", pensamento.id);
-    li.classList.add("li-pensamento");
+    const listaPensamentos = document.getElementById("lista-pensamentos")
+    const li = document.createElement("li")
+    li.setAttribute("data-id", pensamento.id)
+    li.classList.add("li-pensamento")
 
-    const iconeAspas = documento.createElement("img");
-    iconeAspas.src = "assets/imagens/aspas-azuis.png";
-    iconesAspas.alt = "Aspas azuis";
-    iconeAspas.classList.add("icone-aspas");
+    const iconeAspas = document.createElement("img")
+    iconeAspas.src = "assets/imagens/aspas-azuis.png"
+    iconeAspas.alt = "Aspas azuis"
+    iconeAspas.classList.add("icone-aspas")
 
-    const pensamentoConteudo = document.createElement("div");
-    pensamentoConteudo.textContent = pensamento.conteudo;
-    pensamentoConteudo.classList.add("pensamento-conteudo");
+    const pensamentoConteudo = document.createElement("div")
+    pensamentoConteudo.textContent = pensamento.conteudo
+    pensamentoConteudo.classList.add("pensamento-conteudo")
 
-    const pensamentoAutoria = document.createElement("div");
-    pensamentoAutoria.textContent = pensamento.autoria;
-    pensamentoAutoria.classList.add("pensamento-autoria");
+    const pensamentoAutoria = document.createElement("div")
+    pensamentoAutoria.textContent = pensamento.autoria
+    pensamentoAutoria.classList.add("pensamento-autoria")
 
-    li.appendChild(iconeAspas);
-    li.appendChild(pensamentoConteudo);
-    li.appendChild(pensamentoAutoria);
-    listaPensamentos.appendChild(li);
-  },
-};
+    li.appendChild(iconeAspas)
+    li.appendChild(pensamentoConteudo)
+    li.appendChild(pensamentoAutoria)
+    listaPensamentos.appendChild(li)
+  }
+}
 
-export default ui;
+export default ui
